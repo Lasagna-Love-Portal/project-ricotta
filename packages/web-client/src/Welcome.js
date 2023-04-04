0da1ba71be3c53d0cb7954b1c8c3224d243a5e4a
allyson_s <allysonsmithstudio@gmail.com>
2023-04-04 16:16:10 -0400

    Added images and carousel and basic structure of welcome screen

--- packages/web-client/src/Welcome.js
+++ packages/web-client/src/Welcome.js
@@ -1,6 +1,37 @@
 import React from "react";
-//import { Link } from "react-router-dom";
 import logo from "./images/logo-vrt1.png";
+import Carousel from "react-material-ui-carousel";
+import { Paper } from "@mui/material";
+
+const welcomeSlides = [
+  {
+    img: require("./images/rhiannon-cooking.png"),
+    heading: "Get Ready to Spread Kindness!",
+    description:
+      "Positively impact your community by cooking a homemade meal for your neighbor in need.",
+    alt: "Woman and child in kitchen cooking",
+  },
+  {
+    img: require("./images/lasagna.png"),
+    heading: "Earn LL Points",
+    description:
+      "1 lasagna delivered = 1 point. The more points you have the more benefits you’ll receive.",
+    alt: "Lasagna topped with heart shaped cheese and basil flowers",
+  },
+  {
+    img: require("./images/group.png"),
+    heading: "Donate to Help",
+    description: `If you can’t cook one week donate your funds for cooking that week. Any help counts.`,
+    alt: "Eight people stand smiling holding a Lasagna Love banner",
+  },
+  {
+    img: require("./images/kelly-a.png"),
+    heading: "Sign Up For Events",
+    description:
+      "Check your events tab in your matches view to see all upcoming events you could volounter with.",
+    alt: "Man and woman standing before packaged food dressed in Lasagna Love t shirts",
+  },
+];
 
 export default function Welcome() {
   return (
@@ -8,8 +39,20 @@ export default function Welcome() {
       <header>
         <img src={logo} alt="Logo" />
       </header>
-
-      <p>Already have an account? </p>
+      <Carousel>
+        {welcomeSlides.map((item, i) => {
+          return (
+            <Paper key={item.i}>
+              <img src={item.img} alt={item.alt} />
+              <h1>{item.heading}</h1>
+              <p>{item.description}</p>
+              <p>
+                Already have an account? <span>Log In</span>
+              </p>
+            </Paper>
+          );
+        })}
+      </Carousel>
     </div>
   );
 }
