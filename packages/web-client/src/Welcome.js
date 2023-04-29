8454f91125a2d33bd3890fedb69c8744c28f2619
Jonathan Roemer <dev@roemersoftworks.com>
2023-04-29 11:18:11 -0500

    Linting cleanup

--- packages/web-client/src/Welcome.js
+++ packages/web-client/src/Welcome.js
@@ -8,32 +8,32 @@ import ReusableButton from "./components/ReusableButton";
 
 const welcomeSlides = [
   {
-    img: require("./images/rhiannon-cooking.png"),
-    heading: "Get Ready to Spread Kindness!",
+    img: require('./images/rhiannon-cooking.png'),
+    heading: 'Get Ready to Spread Kindness!',
     description:
-      "Positively impact your community by cooking a homemade meal for your neighbor in need.",
-    alt: "Woman and child in kitchen cooking",
+      'Positively impact your community by cooking a homemade meal for your neighbor in need.',
+    alt: 'Woman and child in kitchen cooking',
   },
   {
-    img: require("./images/lasagna.png"),
-    heading: "Earn LL Points",
+    img: require('./images/lasagna.png'),
+    heading: 'Earn LL Points',
     description:
-      "1 lasagna delivered = 1 point. The more points you have the more benefits you'll receive.",
-    alt: "Lasagna topped with heart shaped cheese and basil flowers",
+      '1 lasagna delivered = 1 point. The more points you have the more benefits you\'ll receive.',
+    alt: 'Lasagna topped with heart shaped cheese and basil flowers',
   },
   {
-    img: require("./images/group.png"),
-    heading: "Donate to Help",
+    img: require('./images/group.png'),
+    heading: 'Donate to Help',
     description: 
-      "If you can't cook one week donate your funds for cooking that week. Any help counts.",
-    alt: "Eight people stand smiling holding a Lasagna Love banner",
+      'If you can\'t cook one week donate your funds for cooking that week. Any help counts.',
+    alt: 'Eight people stand smiling holding a Lasagna Love banner',
   },
   {
-    img: require("./images/kelly-a.png"),
-    heading2: "Sign Up For Events",
+    img: require('./images/kelly-a.png'),
+    heading2: 'Sign Up For Events',
     description:
-      "Check your events tab in your matches view to see all upcoming events you could volunteer with.",
-    alt: "Man and woman standing before packaged food dressed in Lasagna Love t shirts",
+      'Check your events tab in your matches view to see all upcoming events you could volunteer with.',
+    alt: 'Man and woman standing before packaged food dressed in Lasagna Love t shirts',
   },
 ];
 
