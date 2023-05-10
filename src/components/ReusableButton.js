4483304f4ac9cae610581d5c17b9bdb3963194a5
mattkuznicki-ll <mattk@lasagnalove.org>
2023-05-10 17:51:04 -0500

    Suggested syntax touchups from linter, pt. 2

--- src/components/ReusableButton.js
+++ src/components/ReusableButton.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { Button } from "@mui/material";
+import React from 'react'
+import { Button } from '@mui/material'
 
 const ReusableButton = (props) => {
   return (
     <Button variant={props.buttonVariant} color={props.buttonColor}>
       {props.buttonText}
     </Button>
-  );
-};
-export default ReusableButton;
+  )
+}
+export default ReusableButton
