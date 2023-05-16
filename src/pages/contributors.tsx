d9dd8873f143cbe49bb2bb3e3f7202373a13fca6
mattkuznicki-ll <mattk@lasagnalove.org>
2023-05-15 20:07:20 -0500

    Remove unneeded imports & reformat

--- src/pages/contributors.tsx
+++ src/pages/contributors.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import React from "react";
 import { useTheme } from "@mui/material/styles";
-import { Paper, Card, Typography, Link } from "@mui/material";
+import { Typography } from "@mui/material";
 import styles from '@/styles/Contributors.module.css';
 const inter = Inter({ subsets: ['latin'] })
 
@@ -50,43 +50,39 @@ const project_ricotta_contributors = [
 ];
 
 export default function Contributors() {
-  const theme = useTheme();
-  return (
-    <main>
-      <header>
-        <Image src={"logo-vrt1.png"} alt="Lasagna Love logo" width={90} height={79} className="logo" />
-      </header>
+    const theme = useTheme();
+    return (
+        <main>
+            <header>
+                <Image src={"logo-vrt1.png"} alt="Lasagna Love logo" width={90} height={79} className="logo" />
+            </header>
 
-      <h1>Lasagna Love Project Ricotta Contributors</h1>
+            <h1>Lasagna Love Project Ricotta Contributors</h1>
 
-      <div>
-        <Typography>
-          <p>
-            Thank you to all who were so generous with their time and skills. We&apos;ve
-            listed the Contributors below. Clicking on their names will direct you to
-            their contribution page.
-          </p>
-          <h2>Contributors</h2>
-          <div className="contributors">
-              <div className="styles.cbox">
-
-                { project_ricotta_contributors.map((item, i) => {
-                    return (
-                    <div className="cname" key={i}>
-                        <a href={item.contrib_url}>{item.cname}</a>
-                    </div>
-                    );
-                })}
-
+            <div className="styles.cbox">
+                <Typography>
+                    <p>
+                        Thank you to all who were so generous with their time and skills. We&apos;ve
+                        listed the Contributors below. Clicking on their names will direct you to
+                        their contribution page.
+                    </p>
+                    <h2>Contributors</h2>
+                    <Typography>
+                        {project_ricotta_contributors.map((item, i) => {
+                            return (
+                                <div className="cname" key={i}>
+                                    <a href={item.contrib_url}>{item.cname}</a>
+                                </div>
+                            );
+                        })}
+                    </Typography>
+                    <p>
+                        If you were a Contributor and your name is not listed, please open a pull request
+                        to enter your name and contribution. We want to honor <i>all</i>
+                        of our Contributors.
+                    </p>
+                </Typography>
             </div>
-          </div>
-          <p>
-            If you were a Contributor and your name is not listed, please open a pull request
-            to enter your name and contribution. We want to honor <i>all</i>
-            of our Contributors.
-          </p>
-        </Typography>
-      </div>
-    </main>
-  );
+        </main>
+    );
 }
