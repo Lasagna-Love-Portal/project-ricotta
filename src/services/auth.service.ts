11bad4961b32f3ee1fab31d78efe92e10cffa6a7
mattkuznicki-ll <mattk@lasagnalove.org>
2023-05-31 16:27:31 -0500

    Refactor token storage

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,27 +1,30 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 // TODO: un hard-code the URL string, obtain from elsewhere
 const API_URL = "http://localhost:8080/";
 
 class AuthService {
+    private storeTokens(loginResponse : AxiosResponse) {
+        if (localStorage.getItem("bechamel_access_token"))
+        {
+            localStorage.removeItem("bechamel_access_token");
+        }
+        if (localStorage.getItem("bechamel_refresh_token"))
+        {
+            localStorage.removeItem("bechamel_refresh_token");
+        }
+        localStorage.setItem("bechamel_access_token", loginResponse.data['access_token']);
+        localStorage.setItem("bechamel_refresh_token", loginResponse.data['refresh_token']);
+    }
+
     async login(username: string, password: string) {
         try {
             let loginResponse = await axios.post(API_URL + "login", { username, password });
             if (loginResponse.status == 200 &&
                 loginResponse.data['access_token'] &&
                 loginResponse.data['refresh_token']) {
-                if (localStorage.getItem("bechamel_access_token"))
-                {
-                    localStorage.removeItem("bechamel_access_token");
-                }
-                if (localStorage.getItem("bechamel_refresh_token"))
-                {
-                    localStorage.removeItem("bechamel_refresh_token");
-                }
-                localStorage.setItem("bechamel_access_token", loginResponse.data['access_token']);
-                localStorage.setItem("bechamel_refresh_token", loginResponse.data['refresh_token']);
-
-                return loginResponse.data['access_token'];
+                    this.storeTokens(loginResponse);
+                    return loginResponse.data['access_token'];
             }
             else if (loginResponse.status == 401) { // unauthorized
                 console.error("Invalid username and password supplied.");
@@ -39,18 +42,8 @@ class AuthService {
             if (loginResponse.status == 200 &&
                 loginResponse.data['access_token'] &&
                 loginResponse.data['refresh_token']) {
-                if (localStorage.getItem("bechamel_access_token"))
-                {
-                    localStorage.removeItem("bechamel_access_token");
-                }
-                if (localStorage.getItem("bechamel_refresh_token"))
-                {
-                    localStorage.removeItem("bechamel_refresh_token");
-                }
-                localStorage.setItem("bechamel_access_token", loginResponse.data['access_token']);
-                localStorage.setItem("bechamel_refresh_token", loginResponse.data['refresh_token']);
-
-                return loginResponse.data['access_token'];
+                    this.storeTokens(loginResponse);
+                    return loginResponse.data['access_token'];
             }
             else if (loginResponse.status == 401) { // unauthorized
                 console.error("Refresh token not authorized, HTTP 401 returned");
