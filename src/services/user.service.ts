7bf2fe50b061cf8f094c2325bc3b216f99b8ad49
emmanolan <51708498+emmanolan@users.noreply.github.com>
2023-08-22 19:35:38 -0400

    idk lets see i hate ts

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -7,7 +7,7 @@ import { RecipientInfo } from '@/model/RecipientInfo';
 import { Profile } from '@/model/Profile';
 
 // TODO: un hard-code the URL string, obtain from elsewhere
-const API_URL = ${{ secrets.API_URL }};
+const API_URL = ${{ secrets.API_URL }}
 
 class UserService {
     async getCurrentUserProfile() : Promise<Profile> {
