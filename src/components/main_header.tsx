e8f4712272c6a912940f4b54d5e16ff2878c8790
funbunch <shannan_bunch@hotmail.com>
2023-10-21 12:32:35 -0700

    Adding key prop

--- src/components/main_header.tsx
+++ src/components/main_header.tsx
@@ -39,7 +39,7 @@ export default function MainHeader () {
 
   return (
     <header className="bg-[#fffaf7]">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <nav className="flex items-center justify-between p-6 mx-auto max-w-7xl lg:px-8" aria-label="Global">
         <HeaderLogo />
         <div className="flex lg:hidden">
           <button
@@ -48,19 +48,19 @@ export default function MainHeader () {
             onClick={() => setMobileMenuOpen(true)}
           >
             <span className="sr-only">Open main menu</span>
-            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            <Bars3Icon className="w-6 h-6" aria-hidden="true" />
           </button>
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
-          {navigation.map((item) => (
-            <Link href={item.href} className="text-sm font-semibold leading-6 text-gray-900">{item.name}</Link>
+          {navigation.map((item, i) => (
+            <Link key={i} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">{item.name}</Link>
           ))}
         </div>
       </nav>
 
       <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full px-6 py-6 overflow-y-auto bg-white sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <HeaderLogo />
             <button
@@ -69,14 +69,14 @@ export default function MainHeader () {
               onClick={() => setMobileMenuOpen(false)}
             >
               <span className="sr-only">Close menu</span>
-              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+              <XMarkIcon className="w-6 h-6" aria-hidden="true" />
             </button>
           </div>
-          <div className="mt-6 flow-root">
+          <div className="flow-root mt-6">
             <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                {navigation.map((item) => (
-                  <Link href={item.href} className="text-sm font-semibold leading-6 text-gray-900">{item.name}</Link>
+              <div className="py-6 space-y-2">
+                {navigation.map((item, i) => (
+                  <Link key={i} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">{item.name}</Link>
                 ))}
               </div>
             </div>
